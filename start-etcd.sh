8fb7601564681d4a10671cc113e58a2265ce3f68
Thanh Phu <git@thanhphu.net>
2020-06-10 12:08:09 +0900

    Working remote start script

--- start-etcd.sh
+++ start-etcd.sh
@@ -3,17 +3,11 @@
 # exit when any command fails
 set -e
 
-go build
-
-scp raftbench ubuntu@raft0:~/raft-bench &
-scp raftbench ubuntu@raft1:~/raft-bench &
-scp raftbench ubuntu@raft2:~/raft-bench
-
 ssh ubuntu@raft0 "rm -rf ~/raft-bench/wal-*"
 ssh ubuntu@raft1 "rm -rf ~/raft-bench/wal-*"
 ssh ubuntu@raft2 "rm -rf ~/raft-bench/wal-*"
 
-ssh ubuntu@raft0 "cd ~/raft-bench && ./raftbench --engine $1 --id 1 --cluster http://10.0.12.142:12379,http://10.0.12.174:12379,http://10.0.12.37:12379  --test" &
-ssh ubuntu@raft1 "cd ~/raft-bench && ./raftbench --engine $1 --id 2 --cluster http://10.0.12.142:12379,http://10.0.12.174:12379,http://10.0.12.37:12379 " &
-ssh ubuntu@raft2 "cd ~/raft-bench && ./raftbench --engine $1 --id 3 --cluster http://10.0.12.142:12379,http://10.0.12.174:12379,http://10.0.12.37:12379 "
+ssh ubuntu@raft0 "cd ~/raft-bench && git pull && /usr/local/go/bin/go build && ./raftbench --engine etcd --id 1 --cluster http://raft0:12379,http://raft1:12379,http://raft2:12379  --test" &
+ssh ubuntu@raft1 "cd ~/raft-bench && git pull && /usr/local/go/bin/go build && ./raftbench --engine etcd --id 2 --cluster http://raft0:12379,http://raft1:12379,http://raft2:12379 " &
+ssh ubuntu@raft2 "cd ~/raft-bench && git pull && /usr/local/go/bin/go build && ./raftbench --engine etcd --id 3 --cluster http://raft0:12379,http://raft1:12379,http://raft2:12379 "
 
--- start-hashi.sh
+++ start-hashi.sh
@@ -3,19 +3,11 @@
 # exit when any command fails
 set -e
 
-go build
-
-scp raftbench ubuntu@raft0:~/raft-bench &
-scp raftbench ubuntu@raft1:~/raft-bench &
-scp raftbench ubuntu@raft2:~/raft-bench
-
 ssh ubuntu@raft0 "rm -rf ~/raft-bench/wal-*"
 ssh ubuntu@raft1 "rm -rf ~/raft-bench/wal-*"
 ssh ubuntu@raft2 "rm -rf ~/raft-bench/wal-*"
 
-ssh ubuntu@raft0 "cd ~/raft-bench && ./raftbench --engine hashi --inmem --nodeid node1 --haddr :11000 --raddr :12000 --test ./wal-hashi" &
-# Let HTTP service start up
-sleep 1
-ssh ubuntu@raft1 "cd ~/raft-bench && ./raftbench --engine hashi --inmem --nodeid node2 --haddr :11000 --raddr :12000 --joinaddr raft0:11000 ./wal-hashi" &
-ssh ubuntu@raft2 "cd ~/raft-bench && ./raftbench --engine hashi --inmem --nodeid node3 --haddr :11000 --raddr :12000 --joinaddr raft0:11000 ./wal-hashi"
+ssh ubuntu@raft0 "cd ~/raft-bench && git pull && /usr/local/go/bin/go build && ./raftbench --engine hashi --inmem --nodeid node1 --haddr raft0:11000 --raddr raft0:12000 ~/raft-bench/wal-hashi" &
+ssh ubuntu@raft1 "cd ~/raft-bench && git pull && /usr/local/go/bin/go build && ./raftbench --engine hashi --inmem --nodeid node2 --haddr raft1:11000 --raddr raft1:12000 --joinaddr raft0:11000 ~/raft-bench/wal-hashi" &
+ssh ubuntu@raft2 "cd ~/raft-bench && git pull && /usr/local/go/bin/go build && ./raftbench --engine hashi --inmem --nodeid node3 --haddr raft2:11000 --raddr raft2:12000 --joinaddr raft0:11000 ~/raft-bench/wal-hashi"
 

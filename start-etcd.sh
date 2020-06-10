1da8b44e21711f83182dff461be562f28b3fb28a
Thanh Phu <git@thanhphu.net>
2020-06-10 10:56:57 +0900

    Restore hashi HTTP service for other nodes to join

--- start-etcd.sh
+++ start-etcd.sh
@@ -9,8 +9,8 @@ scp raftbench ubuntu@raft0:~/raft-bench &
 scp raftbench ubuntu@raft1:~/raft-bench &
 scp raftbench ubuntu@raft2:~/raft-bench
 
-ssh ubuntu@raft0 "rm -rf ~/raft-bench/wal-*" &
-ssh ubuntu@raft1 "rm -rf ~/raft-bench/wal-*" &
+ssh ubuntu@raft0 "rm -rf ~/raft-bench/wal-*"
+ssh ubuntu@raft1 "rm -rf ~/raft-bench/wal-*"
 ssh ubuntu@raft2 "rm -rf ~/raft-bench/wal-*"
 
 ssh ubuntu@raft0 "cd ~/raft-bench && ./raftbench --engine $1 --id 1 --cluster http://10.0.12.142:12379,http://10.0.12.174:12379,http://10.0.12.37:12379  --test" &

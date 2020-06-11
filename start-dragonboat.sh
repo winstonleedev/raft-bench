a7d7612b4d139f2431e335de262611068926dddd
Thanh Phu <git@thanhphu.net>
2020-06-11 10:01:15 +0900

    Adjust retries for dragonboat

--- start-dragonboat.sh
+++ start-dragonboat.sh
@@ -15,7 +15,7 @@ done
 
 sleep 5
 
-ssh ubuntu@raft0 "cd ~/raft-bench && ./raftbench --engine dragonboat --id 1 --mil 1000 --firstWait 5000 --step 1 --test --logfile dragonboat.csv" &
+ssh ubuntu@raft0 "cd ~/raft-bench && ./raftbench --engine dragonboat --id 1 --mil 1000 --firstWait 5000 --step 1 --maxTries 2 --test --logfile dragonboat.csv" &
 ssh ubuntu@raft1 "cd ~/raft-bench && ./raftbench --engine dragonboat --id 2" &
 ssh ubuntu@raft2 "cd ~/raft-bench && ./raftbench --engine dragonboat --id 3"
 
